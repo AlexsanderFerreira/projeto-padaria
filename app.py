c64364cc0458c465c680ddb1d254b3577fedf83d
Alexsander Ferreira <alexsanderferreirailoy@gmail.com>
2024-11-11 12:18:02 -0300

    Implementado a funcionalidade de cadastrar novos produtos

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, request
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///padaria.db"
@@ -17,23 +18,42 @@ class Product(db.Model):
     imagem = db.Column(db.String(100))
 
     def __init__(self, nome: str, descricao: str, ingredientes: str, origem: str, imagem: str) -> None:
-        self._nome = nome
-        self._descricao = descricao
-        self._ingredientes = ingredientes
-        self._origem = origem
-        self._imagem = imagem
+        self.nome = nome
+        self.descricao = descricao
+        self.ingredientes = ingredientes
+        self.origem = origem
+        self.imagem = imagem
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
-@app.route("/listar_produtos")
+@app.route("/listar_produtos", methods=["GET", "POST"])
 def listar_produtos():
-    return render_template("produtos.html")
+    if request.method == "POST":
+        termo = request.form["pesquisa"]
+        resultado = db.session.execute(db.select(Product).filter(Product.nome.like(f'%{termo}%'))).scalars()
+        return render_template('produtos.html', produtos=resultado)
+    else:
+        produtos = db.session.execute(db.select(Product)).scalars()
+        return render_template("produtos.html", produtos=produtos)
 
-@app.route("/cadastrar_produtos")
+@app.route("/cadastrar_produtos", methods=["GET", "POST"])
 def cadastrar_produtos():
-    return render_template("cadastrar.html")
+    if request.method == "POST":
+        status = {"type": "sucesso", "message": "Produto cadastrado com sucesso!"}
+        dados = request.form
+        imagem = request.files['imagem']
+        try:
+            produto = Product(dados['nome'], dados['descricao'], dados['ingredientes'], dados['origem'], imagem.filename)
+            imagem.save(os.path.join('static\imagens', imagem.filename))
+            db.session.add(produto)
+            db.session.commit()
+        except:
+            status = {"type": "Erro", "message": f"Houve um erro ao cadastrar o produto {dados['nome']}!"}
+        return render_template("cadastrar.html", status=status)
+    else:
+        return render_template("cadastrar.html")
 
 if __name__ == '__main__':
     with app.app_context():
